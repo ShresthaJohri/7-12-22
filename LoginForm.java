f43e80a7bcae9ca31cbac0d9b56867871e281d73
Shrestha <shresthajohri35922@gmail.com>
2023-05-09 13:44:14 +0530

    initial commit

---
+++ LoginForm.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+class LoginForm extends Frame
+{
+LoginForm()
+{
+setSize(300,200);
+setLocation(300,200);
+Color c1= new Color(170,234,215);
+setBackground(c1);
+setVisible(true);
+}
+public static void main(String...a1)
+{
+LoginForm l1=new LoginForm();
+}
+}
